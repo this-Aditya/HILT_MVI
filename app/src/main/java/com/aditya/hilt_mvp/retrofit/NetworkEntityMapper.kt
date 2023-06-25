11cfacff1e14c0da2710a9c332d30f1c98b4044a
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-25 22:39:24 +0530

    Added NetworkEntityMapper

---
+++ app/src/main/java/com/aditya/hilt_mvp/retrofit/NetworkEntityMapper.kt
@@ -0,0 +1,19 @@
+package com.aditya.hilt_mvp.retrofit
+
+import com.aditya.hilt_mvp.model.User
+import com.aditya.hilt_mvp.utils.EntityMapper
+import javax.inject.Inject
+
+class NetworkEntityMapper : EntityMapper<UserNetworkEntity, User> {
+
+    override fun mapFromEntity(entity: UserNetworkEntity): User =
+        User(entity.id, entity.name, entity.username, entity.gmail)
+
+    override fun mapToEntity(domainModel: User): UserNetworkEntity =
+        UserNetworkEntity(domainModel.id, domainModel.name, domainModel.username, domainModel.gmail)
+
+    fun mapFromEntityList(entities: List<UserNetworkEntity>): List<User> = entities.map{mapFromEntity(it)}
+
+    fun mapToEntityList(users: List<User>): List<UserNetworkEntity> = users.map {mapToEntity(it)}
+
+}
