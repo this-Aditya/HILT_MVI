1864524600927f11a797a562fb6c7dbef7d2991f
Aditya Mishra <adityamishra2217@gmail.com>
2023-06-29 12:32:19 +0530

    Added retrofit as a hilt dependency

---
+++ app/src/main/java/com/aditya/hilt_mvp/di/RetrofitModule.kt
@@ -0,0 +1,35 @@
+package com.aditya.hilt_mvp.di
+
+import com.aditya.hilt_mvp.retrofit.UserService
+import com.aditya.hilt_mvp.utils.Utils.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+
+    @Singleton
+    @Provides
+    fun provideGson(): Gson = GsonBuilder().excludeFieldsWithoutExposeAnnotation().create()
+
+    @Singleton
+    @Provides
+    fun provideRetrofitBuilder(gson: Gson): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+    fun provideUserService(retrofit: Retrofit): UserService =
+        retrofit.create(UserService::class.java)
+
+}
---
+++ app/src/main/java/com/aditya/hilt_mvp/utils/Utils.kt
@@ -0,0 +1,5 @@
+package com.aditya.hilt_mvp.utils
+
+object Utils {
+    const val BASE_URL = "https://jsonplaceholder.typicode.com"
+}
